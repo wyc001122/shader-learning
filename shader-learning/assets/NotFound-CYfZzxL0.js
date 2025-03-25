4405de0c598dd839a21e2858309e0390568846d9
王泳超 <wangyongchao@testor.com.cn>
2025-03-25 11:17:42 +0800

    feat:更新

---
+++ shader-learning/assets/NotFound-CYfZzxL0.js
@@ -0,0 +1,16 @@
+import{c as l,_ as n}from"./createLucideIcon-Dt8yzaNy.js";import{d as x,c as f,g as t,k as s,u as o,l as r,ab as u,b as m,a3 as c}from"./index-BXMSaq6Y.js";/**
+ * @license lucide-vue-next v0.479.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const p=l("ArrowLeftIcon",[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]]);/**
+ * @license lucide-vue-next v0.479.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const k=l("CircleAlertIcon",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["line",{x1:"12",x2:"12",y1:"8",y2:"12",key:"1pkeuh"}],["line",{x1:"12",x2:"12.01",y1:"16",y2:"16",key:"4dfq90"}]]);/**
+ * @license lucide-vue-next v0.479.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const y=l("HouseIcon",[["path",{d:"M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8",key:"5wwlr5"}],["path",{d:"M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z",key:"1d0kgt"}]]),g={class:"flex flex-col items-center justify-center min-h-screen px-4"},h={class:"flex flex-col items-center text-center space-y-6 max-w-md"},_={class:"rounded-full bg-muted p-6 flex items-center justify-center"},w={class:"flex flex-col sm:flex-row gap-2 mt-2"},H=x({__name:"NotFound",setup(v){const a=u(),i=()=>{a.back()},d=()=>{a.push("/")};return(b,e)=>(m(),f("div",g,[t("div",h,[t("div",_,[s(o(k),{class:"h-12 w-12 text-muted-foreground"})]),e[2]||(e[2]=t("h1",{class:"text-4xl font-bold tracking-tight"},"404",-1)),e[3]||(e[3]=t("h2",{class:"text-2xl font-semibold tracking-tight"},"页面未找到",-1)),e[4]||(e[4]=t("p",{class:"text-muted-foreground"}," 抱歉，您尝试访问的页面不存在或已被移除。 ",-1)),t("div",w,[s(o(n),{onClick:i,variant:"outline",class:"gap-2"},{default:r(()=>[s(o(p),{class:"h-4 w-4"}),e[0]||(e[0]=c(" 返回上一页 "))]),_:1}),s(o(n),{onClick:d,class:"gap-2"},{default:r(()=>[s(o(y),{class:"h-4 w-4"}),e[1]||(e[1]=c(" 返回首页 "))]),_:1})])])]))}});export{H as default};
