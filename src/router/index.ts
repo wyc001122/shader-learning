d399194696ba0c4b7b436d1dc3c0aadd3ca0aab7
王泳超 <wangyongchao@testor.com.cn>
2025-03-26 11:45:37 +0800

    feat:更新

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import menuData from '@/data/menu.json'
 
 const children: any[] = []
@@ -47,7 +47,7 @@ const routes = [
 
 // 创建路由实例
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
 })
 
