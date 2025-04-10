738f263ee33e77af9e54d5cdc1b9e9fdc5fd32d1
王泳超 <wangyongchao@testor.com.cn>
2025-04-10 11:53:50 +0800

    fix: h标签样式异常

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './assets/index.css'
 import router from './router'
 import App from './App.vue'
 import 'highlight.js/styles/github.css'  // 改为 GitHub 风格
-import './assets/markdown.css'  // 引入 Markdown 样式
+import './assets/markdown.scss'  // 引入 Markdown 样式
 import 'viewerjs/dist/viewer.css';
 import VueViewer from 'v-viewer';
 
